4cca3db62b31ddb2b1897b0c7cbb6836ae0f3d35
Anna Alieksieienko <55431139+annalieks@users.noreply.github.com>
2023-03-05 17:07:12 +0100

    Add landing page (#2)

---
+++ src/reducers.js
@@ -0,0 +1,7 @@
+import {combineReducers} from "redux";
+import {reducer as toastr} from 'react-redux-toastr';
+// import home from './scenes/HomePage/reducer';
+
+export default combineReducers({
+    toastr,
+});
---
+++ src/sagas.js
@@ -0,0 +1,8 @@
+import {all} from 'redux-saga/effects';
+// import homeSagas from './scenes/HomePage/sagas';
+
+export default function* rootSaga() {
+    yield all([
+        // homeSagas(),
+    ]);
+}
