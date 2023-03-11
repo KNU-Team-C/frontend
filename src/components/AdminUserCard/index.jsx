20f358555256fb6e66651468d8ca309a28cc53d2
DeLitX <dimkanan@gmail.com>
2023-03-11 22:50:07 +0200

    Added AdminUserCard

---
+++ src/components/AdminUserCard/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './styles.module.sass';
+import {Card} from "semantic-ui-react";
+import classNames from "../../commons/classnames";
+
+
+const AdminUserCard = (props) => {
+    const {username, status, company, onCommunicateClick, onUnbanClick, onBanClick} = props;
+    return (
+        <Card.Group itemsPerRow={1}>
+            <Card>
+                <div className={styles.vertical}>
+                    <div className={styles.horizontal}>
+                        <div>
+                            <img className={styles.user_image}/>
+                        </div>
+                        <div className={styles.vertical}>
+                            <div className={styles.username}>{username}</div>
+                            <div className={styles.horizontal}>
+                                <div className={styles.user_point}>Status:</div>
+                                <div className={styles.user_point_text}>{status}</div>
+                            </div>
+                            <div className={styles.horizontal}>
+                                <div className={styles.user_point}>Company:</div>
+                                <div className={styles.user_point_text}>{company}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.button_details}>Details</div>
+                    <div className={styles.horizontal}>
+                        <div className={classNames(styles.button_communicate, styles.button_common)}
+                             onClick={onCommunicateClick}>
+                            Communicate
+                        </div>
+                        <div className={classNames(styles.button_unban, styles.button_common)}
+                             onClick={onUnbanClick}>
+                            Unban
+                        </div>
+                        <div className={classNames(styles.button_ban, styles.button_common)}
+                             onClick={onBanClick}>
+                            Ban
+                        </div>
+                    </div>
+                </div>
+            </Card>
+        </Card.Group>
+    );
+}
+
+export default AdminUserCard;
