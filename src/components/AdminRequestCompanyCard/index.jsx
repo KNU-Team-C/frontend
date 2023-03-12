753b1f7abc8c3b507144aaacd5d8ff935737dfbb
DeLitX <dimkanan@gmail.com>
2023-03-12 09:31:04 +0200

    Added AdminRequestCompanyCard

---
+++ src/components/AdminRequestCompanyCard/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './styles.module.sass';
+import AdminCompanyCard from "../AdminCompanyCard";
+
+
+const AdminRequestCompanyCard = (props) => {
+    const {typeOfRequest} = props;
+    return (
+        <AdminCompanyCard {...props}
+                          header={
+                              <div className={styles.horizontal}>
+                                  <div className={styles.type_of_request_title}>Type of Request:</div>
+                                  <div className={styles.type_of_request_text}>{typeOfRequest}</div>
+                              </div>
+                          }
+        />
+    );
+}
+
+export default AdminRequestCompanyCard;
