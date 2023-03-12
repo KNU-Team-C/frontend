3af0118fe586ff5e3b19f2829dbd9b93d06ce596
DeLitX <dimkanan@gmail.com>
2023-03-12 09:30:54 +0200

    Added AdminCompanyCard

---
+++ src/components/AdminCompanyCard/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styles from './styles.module.sass';
+import {Card} from "semantic-ui-react";
+import classNames from "../../commons/classnames";
+
+
+const AdminCompanyCard = (props) => {
+    const {
+        header,
+        companyName,
+        status,
+        industries,
+        technologies,
+        details,
+        onCommunicateClick,
+        onVerifyClick,
+        onDeclineClick
+    } = props;
+    return (
+        <Card.Group itemsPerRow={1}>
+            <Card>
+                <div className={styles.vertical}>
+                    {header}
+                    <div className={styles.horizontal}>
+                        <div>
+                            <img className={styles.request_image}/>
+                        </div>
+                        <div className={styles.vertical}>
+                            <div className={styles.company_name}>{companyName}</div>
+                            <div className={styles.horizontal}>
+                                <div className={styles.request_point}>Status:</div>
+                                <div className={styles.default_text}>{status}</div>
+                            </div>
+                            <div className={styles.horizontal}>
+                                <div className={styles.request_point}>Industries:</div>
+                                <div className={styles.default_text}>{industries}</div>
+                            </div>
+                            <div className={styles.horizontal}>
+                                <div className={styles.request_point}>Technologies:</div>
+                                <div className={styles.default_text}>{technologies}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.button_details}>{details}</div>
+                    <div className={styles.horizontal}>
+                        <div className={classNames(styles.button_communicate, styles.button_common)}
+                             onClick={onCommunicateClick}>
+                            Communicate
+                        </div>
+                        <div className={classNames(styles.button_unban, styles.button_common)}
+                             onClick={onVerifyClick}>
+                            Verify
+                        </div>
+                        <div className={classNames(styles.button_ban, styles.button_common)}
+                             onClick={onDeclineClick}>
+                            Decline
+                        </div>
+                    </div>
+                </div>
+            </Card>
+        </Card.Group>
+    );
+}
+
+export default AdminCompanyCard;
