9bb5a179ed02a4a61b2a718b87a4b76aad841741
DeLitX <dimkanan@gmail.com>
2023-04-01 16:30:49 +0300

    Added functionality for admin companies screen

--- src/reducers.js
+++ src/reducers.js
@@ -2,10 +2,12 @@ import {combineReducers} from "redux";
 import {reducer as toastr} from 'react-redux-toastr';
 import authData from './scenes/SignInPage/reducer';
 import companiesData from './scenes/CompaniesPage/reducer';
+import authCompaniesData from "./scenes/AdminCompanySearch/reducer";
 // import home from './scenes/HomePage/reducer';
 
 export default combineReducers({
     toastr,
     authData,
     companiesData,
+    authCompaniesData,
 });
--- src/sagas.js
+++ src/sagas.js
@@ -2,11 +2,13 @@ import {all} from 'redux-saga/effects';
 import signInSagas from './scenes/SignInPage/sagas';
 import signUpSagas from './scenes/SignUpPage/sagas';
 import companiesSagas from './scenes/CompaniesPage/sagas';
+import authCompaniesSagas from "./scenes/AdminCompanySearch/sagas";
 
 export default function* rootSaga() {
     yield all([
         signInSagas(),
         signUpSagas(),
         companiesSagas(),
+        authCompaniesSagas(),
     ]);
 }
