af6b974f0f2d814ab32775478ba97d135b1f1a5b
Ann Alieksieienko <ann.alieksieienko@gmail.com>
2023-04-15 18:08:23 +0300

    Fix Administration button in header

--- src/helpers/token.helper.js
+++ src/helpers/token.helper.js
@@ -1,11 +1,13 @@
-import {TOKEN_NAME, USER_ID} from '../commons/constants';
+import {IS_STAFF, TOKEN_NAME, USER_ID} from '../commons/constants';
 
-export const setToken = (token, userId) => {
+export const setToken = (token, userId, isStaff) => {
     localStorage.setItem(TOKEN_NAME, token);
     localStorage.setItem(USER_ID, userId);
+    localStorage.setItem(IS_STAFF, isStaff);
 };
 
 export const clearToken = () => {
     localStorage.removeItem(TOKEN_NAME);
     localStorage.removeItem(USER_ID);
+    localStorage.removeItem(IS_STAFF)
 };
